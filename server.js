c6061f631d9adc331192e03247699a23b3805d48
metinfbz2 <metinfbz@gmail.com>
2023-08-14 20:10:26 +0200

    Your commit message here 8

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
@@ -12,6 +13,23 @@ connectDB();
 const transactions = require('./routes/transactions');
 
 const app = express();
+
+// for CORS policy
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+// CORS policy end
+
 
 app.use(express.json());
 
